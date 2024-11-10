3315022dff15192760d9215def4dd6aa6ec306b7
Pablo Carballo <pablodcar@gmail.com>
2024-11-11 00:36:28 +0100

    apt command

--- setup-linux-apt.sh
+++ setup-linux-apt.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo apt-get update -y
-sudo apt-get install libssl-dev swig -y
+sudo apt update -y
+sudo apt install libssl-dev swig -y
 
 echo "Config libs: ${pkg-config --libs openssl}"
