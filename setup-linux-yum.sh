06400a56088c09ae0587a0389dc11554c66eff80
Pablo Carballo <pablodcar@gmail.com>
2024-11-11 00:09:06 +0100

    replace centos mirrors

--- setup-linux-yum.sh
+++ setup-linux-yum.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# https://serverfault.com/questions/1161816/mirrorlist-centos-org-no-longer-resolve/1161847#1161847
+sed -i s/mirror.centos.org/vault.centos.org/g /etc/yum.repos.d/CentOS-*.repo
+sed -i s/^#.*baseurl=http/baseurl=http/g /etc/yum.repos.d/CentOS-*.repo
+sed -i s/^mirrorlist=http/#mirrorlist=http/g /etc/yum.repos.d/CentOS-*.repo
+
 yum update -y
 yum install openssl-devel swig -y
 
